2a6ba9ffd29e17d2e9971a5ec4eda0cb690b0c62
Gopinath-at-174881232236 <gopinath.v2507@gmail.com>
2025-07-12 22:15:30 +0530

    Implement basic authentication in FastAPI application and update requirements

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from typing import Annotated
+import bcrypt
+
 
 app = FastAPI()
 
 security = HTTPBasic()
 username = "gopi"
 password = "password"
+hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+print(hash)
+
 
 @app.get("/")
-async def baisc_auth():
-    return {"message": "Welcome to the FastAPI application!"}
+async def basic_auth(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if credentials.username == username:
+        password_check = bcrypt.checkpw(credentials.password.encode('utf-8'), hash)
+        if password_check:    
+            return {"message": "Welcome to the FastAPI application!"}
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password", headers={"WWW-Authenticate": "Basic"})
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials", headers={"WWW-Authenticate": "Basic"})
 
 if __name__ == "__main__":
     import uvicorn
